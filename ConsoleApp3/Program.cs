8d3204b404b9ca85d6b7d61b703e1b08256f7f78
Administrator <Administrator@PC-20190606DDBZ>
2020-07-17 22:16:22 +0800

    First add

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Mammal
+    {
+        public Mammal(string name)
+        {
+            Console.WriteLine("Base:" + name);
+        }
+    }
+
+    class Horse:Mammal
+    {
+        public Horse(string name):base(name)
+        {
+            Console.WriteLine("myHorse:" + name);
+        }
+    }
+
+    class Whale:Mammal
+    {
+        public Whale(string name) : base(name)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Horse myHorse = new Horse("xiao bai!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
